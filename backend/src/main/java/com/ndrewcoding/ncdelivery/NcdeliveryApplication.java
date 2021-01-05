2d5e9066163b5921b44ccaa36eac1a535ef0a873
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-01-04 21:19:08 -0300

    Project created

---
+++ backend/src/main/java/com/ndrewcoding/ncdelivery/NcdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.ndrewcoding.ncdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcdeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ndrewcoding/ncdelivery/NcdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndrewcoding.ncdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
